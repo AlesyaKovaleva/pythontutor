7508d7a0a5abf63d827c792ed4b528e939d9e809
Alesya Kovaleva <kovaleva.alesya@gmail.com>
2016-10-24 21:03:28 +0300

    Add description of tasks

--- task1_5.py
+++ task1_5.py
@@ -1,7 +1,12 @@
+# n школьников делят k яблок поровну, неделящийся остаток остается в корзинке.
+# Сколько яблок достанется каждому школьнику?
+# Сколько яблок останется в корзинке?
+# Программа получает на вход числа n и k и должна вывести
+# искомое количество яблок (два числа).
+
 schoolkids = int(input())
 apples = int(input())
 apples_to_schoolkids = apples // schoolkids
 apples_in_basket = apples % schoolkids
 print(apples_to_schoolkids)
 print(apples_in_basket)
-
